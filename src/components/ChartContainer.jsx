52f96967a156b9dfb4c0005290a51a33942b9b90
Stanisław Marciniak <marciniak.dev@gmail.com>
2024-10-26 04:31:40 +0200

    heart rate chart

--- src/components/ChartContainer.jsx
+++ src/components/ChartContainer.jsx
@@ -1,14 +1,12 @@
 import { Flex, Heading, Stack } from "@chakra-ui/react";
 import React from "react";
-import SelectTime from "../SelectTime";
+import SelectTime from "./SelectTime";
 
 const ChartContainer = ({ title, ChartComponent }) => {
   return (
-    <Stack>
+    <Stack gap={3}>
       <Flex gap={4} alignItems="center">
-        <Heading fontSize="xl" mb={2}>
-          {title}
-        </Heading>
+        <Heading fontSize="xl">{title}</Heading>
         <SelectTime />
       </Flex>
       <ChartComponent />
--- src/components/ChartGrid.jsx
+++ src/components/ChartGrid.jsx
@@ -1,19 +1,31 @@
 import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
 import React from "react";
+import ChartContainer from "./ChartContainer";
+import HeartRateBarChart from "./charts/HeartRateBarChart";
 
 const ChartGrid = () => {
   return (
-    <Flex direction="column" gap={4} mt={3}>
-      <Flex gap={1}>
-        <Box h="300px" w="600px" bg="gray.300" rounded="xl"></Box>
+    <Flex direction="column" gap={10} mt={3}>
+      <Flex gap={5}>
+        <ChartContainer title="Heart Rate" ChartComponent={HeartRateBarChart} />
+        <ChartContainer
+          title="Average Steps"
+          ChartComponent={HeartRateBarChart}
+        />
       </Flex>
-      <Flex gap={1}>
-        <Box h="300px" w="600px" bg="gray.300" rounded="xl"></Box>
-        <Box h="300px" w="600px" bg="gray.300" rounded="xl"></Box>
+      <Flex gap={5}>
+        <ChartContainer title="Sleep Time" ChartComponent={HeartRateBarChart} />
+        <ChartContainer title="Mood" ChartComponent={HeartRateBarChart} />
       </Flex>
-      <Flex gap={1}>
-        <Box h="300px" w="600px" bg="gray.300" rounded="xl"></Box>
-        <Box h="300px" w="600px" bg="gray.300" rounded="xl"></Box>
+      <Flex gap={5}>
+        <ChartContainer
+          title="Sport activity"
+          ChartComponent={HeartRateBarChart}
+        />
+        <ChartContainer
+          title="Systematic breaks"
+          ChartComponent={HeartRateBarChart}
+        />
       </Flex>
     </Flex>
   );
